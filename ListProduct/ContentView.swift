14f0ce09b9a7634bfe218d43b07269180ad58487
Forkipe <mhorkiy@gmail.com>
2022-09-20 22:49:55 +0300

    final

--- ListProduct/ContentView.swift
+++ ListProduct/ContentView.swift
@@ -8,11 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var listItems = [Item]()
+
+        @State var addItemName: String = ""
+
+        @State var colorSelection: UIColor = UIColor.systemRed
+
+        @State var addItem = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+                   VStack {
+                       NavigationView {
+                           List {
+                               ForEach(listItems, id: \.self) { item in
+                                   Text(item.name).listRowBackground(Color(item.color))
+                               }
+                           }
+                               .navigationBarTitle(Text("Products"))
+                              
+                               .navigationBarItems(trailing:
+                                   Button(action: {
+                                       self.addItem.toggle()
+                                   }, label: {
+                                       Image(systemName: "plus")
+                                   })
+
+                           )
+                       }.sheet(isPresented: $addItem) {
+                           VStack {
+                               HStack {
+                                   Text("Item Name: ")
+                                   TextField("Type...", text: self.$addItemName)
+                               }
+                               Picker(selection: self.$colorSelection, label: Text("Select a Color")) {
+                                   Text("Red").tag(UIColor.systemRed)
+                                   Text("Blue").tag(UIColor.systemBlue)
+                                   Text("Green").tag(UIColor.systemGreen)
+                                   Text("Yellow").tag(UIColor.systemYellow)
+                                   Text("Brown").tag(UIColor.systemBrown)
+                               }
+                               Spacer()
+                                   .frame(height: 20.0)
+                               Button(action: {
+                                   self.listItems.append(Item(name: self.addItemName, color: self.colorSelection))
+
+                                 
+                                   self.addItem.toggle()
+                                   self.addItemName = ""
+                                   self.colorSelection = UIColor.systemRed
+                               }, label: {
+                                   Text("Add")
+                                   . frame(minWidth: 0, maxWidth: . infinity)
+                                   . font(. system(size: 15))
+                                   . padding()
+                                   . foregroundColor(. black)
+                                   .overlay(
+                                    RoundedRectangle(cornerRadius: 15) .stroke(Color.red, lineWidth: 2))
+                                   .frame(width: 70.0, height: 20.0)
+                                       
+                                   
+                               })
+                           }.padding(100)
+                       }
+
+               }
+           }
+       }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ ListProduct/ItemsUI.swift
@@ -0,0 +1,14 @@
+//
+//  ItemsUI.swift
+//  ListProduct
+//
+//  Created by Марк Горкій on 20.09.2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct Item: Hashable {
+    let name: String
+    let color: UIColor
+}
