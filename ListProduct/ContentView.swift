9fe29fec9db9e988049ed28c410d5cbed709ee5e
Forkipe <mhorkiy@gmail.com>
2022-09-20 20:16:33 +0300

    Initial Commit

---
+++ ListProduct/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListProduct
+//
+//  Created by Марк Горкій on 20.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListProduct/ListProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListProductApp.swift
+//  ListProduct
+//
+//  Created by Марк Горкій on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ListProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
